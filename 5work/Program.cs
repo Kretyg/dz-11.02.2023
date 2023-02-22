0c4c420464ed3098ba20d88e8dc290ffe7cf4cf7
Kretyg <sokolovsky.zhora58@gmail.com>
2023-02-22 05:08:09 +0300

    add 5 work

---
+++ 5work/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+int N = 10;
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int Metod(int x, int y, int num)
+{
+    if (num == 1) return x;
+    if (num == 2) return y;
+    return Metod(x, y, num - 1) + Metod(x, y, num - 2);
+}
+for (int i = 1; i < N; i++)
+{
+    Console.Write(Metod(a, b, i) + " ");
+}
