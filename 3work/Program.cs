a469dc71cdfff386a0e8d0196063623fd4ca250f
Kretyg <sokolovsky.zhora58@gmail.com>
2023-02-22 05:07:33 +0300

    add 3 work

---
+++ 3work/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+void PrintNumber(int m, int n)
+{
+    if (n < m) return;
+    PrintNumber(m, n - 1);
+    Console.Write(n + " ");
+}
+
+int Sum(int m, int n)
+{
+    if (n == m) return n;
+    return m + Sum(m + 1, n);
+}
+PrintNumber(1, 10);
+Console.WriteLine();
+Console.WriteLine(Sum(1, 10));
